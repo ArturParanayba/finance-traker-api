3f5b247bc10037e7cd79ee6df9128aa6e48e1f68
Artur Paranayba <artur.paranayba@citechnology.com.br>
2023-09-07 21:32:49 -0300

    helmet added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +14,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.use(helmet());
 
   await app.listen(3000);
 }
